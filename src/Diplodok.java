2c4b41c291926a9c1eed9fcfbe7995d0cd7a9f0f
Daniyar <mdaniyar1995@gmail.com>
2022-01-27 19:35:15 +0300

    1

---
+++ src/Diplodok.java
@@ -0,0 +1,7 @@
+public class Diplodok implements Eatable {
+
+    @Override
+    public void eat() {
+        System.out.println("Диплодок ест траву");
+    }
+}
---
+++ src/Tiranozavr.java
@@ -0,0 +1,7 @@
+public class Tiranozavr implements Eatable {
+
+    @Override
+    public void eat() {
+        System.out.println("Тиранозавр ест диплодока");
+    }
+}
